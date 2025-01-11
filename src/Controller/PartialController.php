5ada975052faa237d9aeb1aca4335a866a0e734c
Hevitriniavo <hei.tantely@gmail.com>
2025-01-11 23:31:57 +0300

    chore: create view search_replacement

--- src/Controller/PartialController.php
+++ src/Controller/PartialController.php
@@ -7,8 +7,35 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PartialController extends AbstractController
 {
+
     public function index(): Response
     {
-        return $this->render('partials/search_by_country.html.twig');
+        $regions = [
+            ['value' => 'All', 'label' => 'Région'],
+            ['value' => '463', 'label' => 'Alsace'],
+            ['value' => '464', 'label' => 'Aquitaine'],
+        ];
+
+        $specialites = [
+            ['value' => 'All', 'label' => 'Spécialité'],
+            ['value' => '287', 'label' => 'Anatomopathologie'],
+            ['value' => '268', 'label' => 'Anesthésie-réanimation'],
+        ];
+
+        $partenaires = [
+            ['src' => 'images/clinea_new-bleu-rvb_002.jpg', 'alt' => 'Logo Clinea'],
+            ['src' => 'images/ramsay_logo.jpg', 'alt' => 'Logo Ramsay'],
+            ['src' => 'images/elsan_logo.png', 'alt' => 'Logo Elsan'],
+            ['src' => 'images/logo-inicea.jpg', 'alt' => 'Logo Inicea'],
+            ['src' => 'images/logo_c2s_.png', 'alt' => 'Logo C2S'],
+            ['src' => 'images/macsf.png', 'alt' => 'Logo MACSF'],
+        ];
+
+        return $this->render('partials/search_by_country.html.twig', [
+            'regions' => $regions,
+            'specialites' => $specialites,
+            'partenaires' => $partenaires,
+            'nombreRemplacants' => 7873,
+        ]);
     }
 }
---
+++ src/Controller/SearchReplacementController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SearchReplacementController extends AbstractController
+{
+    #[Route('/rechercher-remplacants', name: 'search_replacement')]
+    public function index (Request $request): Response
+    {
+        dump($request->query);
+        return $this->render('search_replacement/index.html.twig');
+    }
+}
